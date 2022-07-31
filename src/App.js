e96feeb073e969418dbf036931e5b95b5df47640
Ryan Gao <ryantchgao@googlemail.com>
2022-07-30 23:25:04 -0700

    create inputs for state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Message from './Message'
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 function App() {
+
+  // Initialize Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZvOFNKfZGUniUfns6KuZimlCHFmf6lVM",
+    authDomain: "chatapp-893e3.firebaseapp.com",
+    projectId: "chatapp-893e3",
+    storageBucket: "chatapp-893e3.appspot.com",
+    messagingSenderId: "307097108957",
+    appId: "1:307097108957:web:6d6a706b87e55d9b74b634",
+    measurementId: "G-2L608MSNPJ",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
+  const [author, setAuthor] = useState("")
+  const [message, setMessage] = useState({user:"Ryan", message: "Hello"})
+
+  const handleSubmit = (e) => {
+    setMessage({ ...message, user: author, message: e.target.value})
+  }
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      set name
+      <input type='text' onChange={(e) => {setAuthor(e.target.value)}} />
+
+      message
+      <input type='text' onChange={handleSubmit} />
+
+      
+
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Message = () => {
+    return (
+        <div>
+             name:
+            message:
+        </div>
+    );
+}
+
+export default Message;
